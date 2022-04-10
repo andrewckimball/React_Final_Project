cdd5b83505757878a7d300880f33d2ab949c6dc0
Andrew Kimball <andrewkimball@macbook-pro-5.lan>
2022-04-10 15:14:28 -0600

    some updates

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 import SearchBar from "./Components/SearchBar.js";
 import senatordata from "./listData.json";
 import JSSoup from 'jssoup';
+import {GoogleMap, withScriptjs, withGoogleMap, Marker} from 'react-google-maps';
 
+/* IMPORTANT youtube vids: https://www.youtube.com/watch?v=x7niho285qs | https://www.youtube.com/watch?v=Gyg5R8Sfo1U */
 
 function TestTweet() {
   const [ search, setSearch ] = useState("");
@@ -18,20 +20,11 @@ function TestTweet() {
     const response_senator = await fetch(sen_name_endpoint);
     
     if (!response_senator.ok) {
-      throw Error(response.statusText);
-    }
-    const senator_wiki_name = await response_senator.json();
-
-    // Pull wikipedia article 
-    const endpoint = `http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=${senator_wiki_name}&origin=*`
-    const response = await fetch(endpoint);
-
-    if (!response.ok) {
-        throw Error(response.statusText);
+      throw Error(response_senator.statusText);
     }
 
-    const json = await response.json()
-    setResults(json.parse.text['*'])
+    const json = await response_senator.json()
+    setResults(json['wiki_html'].slice(1,-1))
   }
 
   return (
@@ -116,6 +109,52 @@ function SelectV2() {
     );
 }
 
+function SelectV3() {
+  const [ search, setSearch ] = useState("");
+  const [ results, setResults ] = useState([]);
+  // const [ searchInfo, setSearchInfo] = useState({});
+
+  const handleSearch = async e => {
+      e.preventDefault();
+      if (search === "") return;
+
+      const endpoint = `http://127.0.0.1:8000/select/${search}`;
+      const response = await fetch(endpoint);
+      
+      if (!response.ok) {
+          throw Error(response.statusText);
+      }
+
+      const json = await response.json()
+
+      if (json[0] === "Error, not found") {
+        setResults("")
+      } else {
+        setResults(json['wiki_html'].slice(1,-1))
+      }
+  }
+
+
+  return (
+      <div className='SelectV2'>
+          <form name="form" class="col-lg-9 offset-lg-1" onSubmit={handleSearch}>
+              <div class="form-group">
+                  <label>Choose Senator among list</label>
+                  <input 
+                      class="form-control" 
+                      name="tweet" 
+                      placeholder="Enter Senator" 
+                      value={search}
+                      onChange={e => setSearch(e.target.value)}/>
+              </div>
+              <button value="Submit" class="btn btn-outline-primary">Search</button>
+          </form> 
+          <div className='result'>
+              <p dangerouslySetInnerHTML={{__html: results}}></p>
+          </div> 
+      </div>
+  );
+}
 
 function Switch(props) {
   const { checked, handleChange } = props;
@@ -138,12 +177,12 @@ function Switch(props) {
 }
 
 function TwitterApp() {
-    const [checked, setChecked] = useState(false);
+  const [checked, setChecked] = useState(false);
 
-    const handleChange = () => {
-        setChecked(!checked);
-    }
-   
+  const handleChange = () => {
+      setChecked(!checked);
+  }
+
   return (
     <div className="App-header">
         <div className='header'>
@@ -157,14 +196,38 @@ function TwitterApp() {
                     <TestTweet/>
                 ) : (
                     // <SelectSenator/>
-                    <SelectV2/>
+                    <SelectV3/>
                 )}
             </div>
             <div className="map">
+              <WrappedMap 
+                googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_KEY}&v=3.exp&libraries=geometry,drawing,places`}
+                loadingElement={<div style={{ height: `100%` }} />}
+                containerElement={<div style={{ height: `100%` }} />}
+                mapElement={<div style={{ height: `100%` }} />}
+              />
             </div>
         </div>
     </div>
   );
 }
+
+function Map() {
+  return (
+    <GoogleMap 
+      defaultZoom={4}
+      defaultCenter={{lat: 38.907192, lng: -77.036873}}
+    >
+      <Marker 
+        position={{
+          lat: 38.907192,
+          lng: -77.036873
+        }}
+      />
+    </GoogleMap>
+  );
+}
+
+const WrappedMap = withScriptjs(withGoogleMap(Map));
 
 export default TwitterApp;
